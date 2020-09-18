1475730313ff3ad690f70d753576b8c8a0f6c260
Minkwang Park <50613287+minkpang@users.noreply.github.com>
2020-09-18 16:43:42 +0900

    Update client-FFinal.py
    python 코드 operation 전후로 공백

--- client-FFinal.py
+++ client-FFinal.py
@@ -24,7 +24,7 @@ BLOCK_SIZE = 16
 HOST = '127.0.1.1'
 PORT = 3000
 BUFSIZE = 1024
-ADDR = (HOST,PORT)
+ADDR = (HOST, PORT)
 
 clientSocket = socket(AF_INET, SOCK_STREAM)# 서버에 접속하기 위한 소켓을 생성한다.
 W=[]
@@ -35,48 +35,48 @@ for i in range(16):
 
 c_cipher = AES.new(Ks.encode("utf8"), AES.MODE_ECB)
 c_decipher = AES.new(Ks.encode("utf8"), AES.MODE_ECB)
-1
+
 files=glob.glob('*.txt')
 for file in files:
     with open(file, 'r') as f:
         while True:
             line=(f.readline().rstrip())
-            if line!='':
+            if line! = '':
                 W.append(line)
-            if not line: break
+            if not line : break
 
 W = list(set(W))
 print(W)
 #w = input("검색값을 입력하세요: ")
 w = "타박상"
 print(w)
-Sero=len(files)
-Garo=len(W)
+Sero = len(files)
+Garo = len(W)
 print(Sero)
 print(Garo)
-File={}
+File = {}
 for j in W:
-    dic=[]
+    dic = []
     for file in files:
         with open(file, 'r') as f:
             if j in open(file).read():
                 dic.append(file[:18])
     print(dic)
-    File[j]=dic
+    File[j] = dic
 values = File.values()
 keys = File.keys()
 
 print(File)
-T={}
+T = {}
 
 for i in keys:
-    el=[]
-    Ke=c_cipher.encrypt(pad(i.encode("utf8"),BLOCK_SIZE))
+    el = []
+    Ke = c_cipher.encrypt(pad(i.encode("utf8"),BLOCK_SIZE))
     for h in File[i]:
         d_cipher = AES.new(Ke, AES.MODE_ECB)
-        e=d_cipher.encrypt(h.encode("utf8"))
+        e = d_cipher.encrypt(h.encode("utf8"))
         el.append(e)
-    T[i]=el
+    T[i] = el
 print(T)
 
 print('****')
@@ -84,11 +84,11 @@ d_decipher = AES.new(Ke, AES.MODE_ECB)
 keys = T.keys()
 values = T.values()
 
-Kt= ""
+Kt = ""
 for i in range(16):
     Kt += random.choice(string_pool)
 #print(Kt)
-Ks=""
+Ks = ""
 for i in range(16):
     Ks += random.choice(string_pool)
 print(Ks)
@@ -100,56 +100,56 @@ from Crypto.Util.Padding import pad, unpad
 print(keys)
 Tset=[[{0:0}]*(Sero+1) for i in range(Garo)]
 for o in keys:
-    stag=a_cipher.encrypt(pad(o.encode("utf8"),BLOCK_SIZE))
+    stag = a_cipher.encrypt(pad(o.encode("utf8"),BLOCK_SIZE))
     print(list)
-    index=0
-    list=[]
-    i=1
+    index = 0
+    list = []
+    i = 1
     for k in T[o]:
 
         print(o)
-        kindex=bin(i)
+        kindex = bin(i)
         b_cipher = AES.new(pad(kindex.encode("utf8"),BLOCK_SIZE), AES.MODE_ECB)
         f = b_cipher.encrypt(stag) # stag 를 한번더 암호화
         m = SHA256.new()
-        hex=m.update(f)
-        H=m.hexdigest()
-        B=int(H,16)
-        b=(bin(B)[213:220])
-        K=(bin(B)[2:131])
-        L=(bin(B)[132:212])
+        hex = m.update(f)
+        H = m.hexdigest()
+        B = int(H,16)
+        b = (bin(B)[213:220])
+        K = (bin(B)[2:131])
+        L = (bin(B)[132:212])
 
-        j=random.randint(0,Sero)
+        j = random.randint(0,Sero)
         while j in list:
-            j=random.randint(0,Sero)
+            j = random.randint(0,Sero)
         list.append(j)
-        n=len(File[o])
-        index=index+1
-        Bcount=1
-        b=(int(b,2)%98)
+        n = len(File[o])
+        index = index+1
+        Bcount = 1
+        b = (int(b,2)%98)
         print(b)
         print(j)
-        if(index==n):Bcount=0
+        if(index == n):Bcount=0
         print(list)
         print(index)
 
-        k1=k.hex()
-        temp=BitArray(hex=k1).bin
-        temp3=str(Bcount)+temp
-        d=""
+        k1 = k.hex()
+        temp = BitArray(hex = k1).bin
+        temp3 = str(Bcount) + temp
+        d = ""
         for q in range(129):
-            if temp3[q]==K[q]:
-                d=d+"0"
+            if temp3[q] == K[q]:
+                d = d + "0"
             else:
-                d=d+"1"
-        K2=d
-        Tset[b][j]={L:K2}
-        i=i+1
+                d = d + "1"
+        K2 = d
+        Tset[b][j] = {L:K2}
+        i = i + 1
 
 print("끝")
-Tset1=str(Tset)
+Tset1 = str(Tset)
 
-stag1=a_cipher.encrypt(pad(w.encode("utf8"),BLOCK_SIZE))
+stag1 = a_cipher.encrypt(pad(w.encode("utf8"), BLOCK_SIZE))
 pTset = pickle.dumps(Tset)
 pstag = pickle.dumps(stag1)
 
@@ -165,21 +165,21 @@ msg = clientSocket.recv(1000000)
 
 print(pickle.loads(msg))
 
-pV=(pickle.loads(msg))
+pV = (pickle.loads(msg))
 
-finallist=[]
-f=pV[0]
+finallist = []
+f = pV[0]
 for i in pV:
 
-    temp=BitArray(bin=i).hex
+    temp = BitArray(bin = i).hex
     print(temp)
-    f1=BitArray(bin=i)
-    f2=bytearray.fromhex(temp)
+    f1 = BitArray(bin = i)
+    f2 = bytearray.fromhex(temp)
     print(f2)
-    Ke=c_cipher.encrypt(pad(w.encode("utf8"),BLOCK_SIZE))
+    Ke = c_cipher.encrypt(pad(w.encode("utf8"), BLOCK_SIZE))
     print(Ke)
     e_decipher = AES.new(Ke, AES.MODE_ECB)
-    idi=e_decipher.decrypt(f2)
+    idi = e_decipher.decrypt(f2)
     finallist.append(idi)
     print(idi)
 
